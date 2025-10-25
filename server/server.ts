1066be7a26aca6159091299e54891ed6c1f3f3e2
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-25 16:16:46 +0100

    Update .gitignore and implement file change handling in server

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,20 @@
 import { watch } from "fs";
+import { readFile } from "fs/promises";
 import path from "path";
 
 const jsonPath = path.join("..", "results.json");
 
+async function handleChange() {
+    const contents = await readFile(jsonPath, "utf-8");
+
+    try {
+        const jsonContents = JSON.parse(contents);
+        console.log(jsonContents);
+    } catch (err) {}
+}
+
 watch(jsonPath, null, (event, filename) => {
     if (event === "change") {
+        handleChange();
     }
 });
